ef3ce966192aaede474bba3ccb7381d90564615e
Saghar <saghar.talebipoor@gmail.com>
2019-11-21 17:39:12 -0800

    added serial communication to web socket client

--- local_server/local_server.py
+++ local_server/local_server.py
@@ -1,6 +1,52 @@
 import socketio
+import serial
+import requests
+import ast
+from flask import Flask, request, jsonify
+import json
+
 
 sio = socketio.Client()
+curr_cursor_place = {'curr_x': 0, 'curr_y': 0}
+ser = serial.Serial()
+ser.baudrate = 115200
+ser.port = '/dev/cu.SLAB_USBtoUART'
+ser.open()
+
+def update_current_cursor_place(new_x, new_y):
+    curr_cursor_place['curr_x'] = new_x
+    curr_cursor_place['curr_y'] = new_y
+
+def find_movement_values(new_x, new_y):
+    x_movement = new_x - curr_cursor_place['curr_x']
+    y_movement = new_y - curr_cursor_place['curr_y']
+    x_values = []
+    y_values = []
+    if x_movement >= 0:
+        while x_movement > 127:
+            x_values.append(127)
+            x_movement = x_movement - 127
+        x_values.append(x_movement)
+    else:
+        while x_movement < -128:
+            x_values.append(128)
+            x_movement = x_movement + 128
+        x_values.append(x_movement + 256)
+    if y_movement >= 0:
+        while y_movement > 127:
+            y_values.append(127)
+            y_movement = y_movement - 127
+        y_values.append(y_movement)
+    else:
+        while y_movement < -128:
+            y_values.append(128)
+            y_movement = y_movement + 128
+        y_values.append(y_movement + 256)
+    while len(x_values) > len(y_values):
+        y_values.append(0)
+    while len(x_values) < len(y_values):
+        x_values.append(0)
+    return x_values, y_values
 
 @sio.event
 def connect():
@@ -9,6 +55,31 @@ def connect():
 @sio.event
 def callback_server2client(data):
     print('message received with ', data)
+    cursor_place = data.split('$')[1]
+    print(cursor_place)
+    x = (cursor_place.split('&')[0]).split('=')[1]
+    y = (cursor_place.split('&')[1]).split('=')[1]
+    input_x = int(float(x))
+    input_y = int(float(y))
+    print(input_x)
+    print(input_y)
+    x_movement_values, y_movement_values = find_movement_values(input_x, input_y)
+    print(x_movement_values)
+    print(y_movement_values)
+    update_current_cursor_place(input_x, input_y)
+    print(curr_cursor_place)
+    for i in range(0, len(x_movement_values)):
+        if i == (len(x_movement_values) - 1):
+            values = bytearray([x_movement_values[i], y_movement_values[i], 1])
+            ser.write(values)
+            values = bytearray([0, 0, 0])
+            ser.write(values)
+        else:
+            values = bytearray([x_movement_values[i], y_movement_values[i], 0])
+            ser.write(values)
+        print("Mouse event sent")
+
+
     # sio.emit('callaback_client2server', {'response': 'my response'})
 
 @sio.event
